ba12d8cecfe9ad65a922006a922ed14dacfbda77
j2sdk_ball2 <nattawut@moneytable.com>
2017-11-11 23:18:03 +0700

    add index component

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+import 'font-awesome/css/font-awesome.min.css'
+import 'simple-line-icons/css/simple-line-icons.css'
+import '../scss/style.scss'
+import '../scss/core/_dropdown-menu-right.scss'
+
+import store from './redux/store'
+import NotFound from './components/errors/NotFound'
+import InternalServerError from './components/errors/InternalServerError'
+import Login from './containers/Login'
+
+const Root = () =>
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/login" name="Login" component={Login} />
+        <Route exact path="/404" name="NotFound" component={NotFound} />
+        <Route exact path="/500" name="InternalServerError" component={InternalServerError} />
+        <Redirect from="/" to="/login" />
+        <Route component={NotFound} />
+      </Switch>
+    </HashRouter>
+  </Provider>
+
+render(<Root />, document.querySelector('react'))
