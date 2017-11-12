3f467ccc1e0965d30b950bacf589583d9c0e347d
j2sdk_ball2 <nattawut@moneytable.com>
2017-11-12 16:06:53 +0700

    refactor

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,2 +1,2 @@
-export const signInUrl = process.env.SIGN_IN_URL || ''
-export const url = (endpoint = '') => `${signInUrl}${endpoint}`
+export const authenUrl = process.env.SIGN_IN_URL || ''
+export const url = (endpoint = '') => `${authenUrl}${endpoint}`
