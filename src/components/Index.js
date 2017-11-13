6071f37d204be67833de19c8911c723f2423cb80
j2sdk_ball2 <nattawut@moneytable.com>
2017-11-13 18:57:31 +0700

    add copyToClipboard button

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import {
   Row,
@@ -12,89 +12,113 @@ import {
   Input,
 } from 'reactstrap'
 import { Icon } from 'react-fa'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const Index = ({ longUrl, shortUrl, shortenUrl, shortening, cancel }) =>
-  <div className="animated fadeIn">
+class Index extends Component {
+  state = { copied: false }
 
-    <Card>
-      <CardHeader>
-        <strong>URL Shortener</strong>
-        <small> enter a long URL to be shorten</small>
-      </CardHeader>
-      <CardBody>
-        <Row>
-          <Col xs="12">
-            <FormGroup>
-              <Label htmlFor="longUrl">Long URL</Label>
-              <Field
-                name="longUrl"
-                component={({ input }) =>
-                  <div>
-                    <Input
-                      {...input}
-                      type="text"
-                      id="longUrl"
-                      placeholder="Long URL"
-                      disabled={shortening}
-                    />
-                  </div>
-                }
-                type="text"
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <div className="form-actions" style={{ textAlign: 'right' }}>
+  render() {
+    const { longUrl, shortUrl, shortenUrl, shortening, cancel } = this.props
 
-          {shortening ?
-            <Button
-              className="px-4"
-              onClick={cancel}
-            >
-              Cancel
-            </Button> : ''
-          }
-          &nbsp;&nbsp;
-          <Button
-            color="primary"
-            className="px-4"
-            disabled={!longUrl || shortening}
-            onClick={() => {
-              if (shortenUrl) {
-                shortenUrl(longUrl)
+    return (
+      <div className="animated fadeIn">
+
+        <Card>
+          <CardHeader>
+            <strong>URL Shortener</strong>
+            <small> enter a long URL to be shorten</small>
+          </CardHeader>
+          <CardBody>
+            <Row>
+              <Col xs="12">
+                <FormGroup>
+                  <Label htmlFor="longUrl">Long URL</Label>
+                  <Field
+                    name="longUrl"
+                    component={({ input }) =>
+                      <div>
+                        <Input
+                          {...input}
+                          type="text"
+                          id="longUrl"
+                          placeholder="Long URL"
+                          disabled={shortening}
+                        />
+                      </div>
+                    }
+                    type="text"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <div className="form-actions" style={{ textAlign: 'right' }}>
+
+              {shortening ?
+                <Button
+                  className="px-4"
+                  onClick={cancel}
+                >
+                  Cancel
+                </Button> : ''
               }
-            }}
-          >
-            <Icon spin name={shortening ? 'circle-o-notch' : ''} size="lg" />&nbsp;&nbsp;Shorten URL
-          </Button>
+              &nbsp;&nbsp;
+              <Button
+                color="primary"
+                className="px-4"
+                disabled={!longUrl || shortening}
+                onClick={() => {
+                  if (shortenUrl) {
+                    shortenUrl(longUrl)
+                  }
+                }}
+              >
+                <Icon spin name={shortening ? 'circle-o-notch' : ''} size="lg" />&nbsp;&nbsp;Shorten URL
+              </Button>
 
-        </div>
-      </CardBody>
-    </Card>
+            </div>
+          </CardBody>
+        </Card>
 
-    <Card>
-      <CardHeader>
-        <strong>Short URL</strong>
-        <small> enter your site with the generated-shorter URL</small>
-      </CardHeader>
-      <CardBody>
-
-        <Col xs="12">
-          <FormGroup>
-            <Input
-              name="shortUrl"
-              type="text"
-              id="shortUrl"
-              placeholder="Short URL"
-              value={shortUrl ? `${window.location.origin}/#/${shortUrl}` : ''}
-              readOnly
-            />
-          </FormGroup>
-        </Col>
-
-      </CardBody>
-    </Card>
-  </div >
+        <Card>
+          <CardHeader>
+            <strong>Short URL</strong>
+            <small> enter your site with the generated-shorter URL</small>
+          </CardHeader>
+          <CardBody>
+            <Row>
+              <Col xs="12">
+                <FormGroup>
+                  <Input
+                    name="shortUrl"
+                    type="text"
+                    id="shortUrl"
+                    placeholder="Short URL"
+                    value={shortUrl ? `${window.location.origin}/#/${shortUrl}` : ''}
+                    readOnly
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <div className="form-actions" style={{ textAlign: 'right' }}>
+              <CopyToClipboard
+                text={shortUrl ? `${window.location.origin}/#/${shortUrl}` : ''}
+                onCopy={() => this.setState({ copied: true })}
+              >
+                <Button
+                  color="primary"
+                  className="px-4"
+                  disabled={!shortUrl}
+                >
+                  Copy to Clipboard
+                </Button>
+              </CopyToClipboard>
+            </div>
+          </CardBody>
+        </Card>
+      </div >
+    )
+  }
+}
 
 export default reduxForm({
   form: 'urlShortenerForm',
